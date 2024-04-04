55f1fe753f9778afb806f5890462ae558aa0681b
hiteshcreator <thehiteshcreator@gmail.com>
2024-04-05 03:18:23 +0530

    social link

--- src/components/SearchBars.jsx
+++ src/components/SearchBars.jsx
@@ -2,13 +2,21 @@ import React,{memo} from 'react'
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
 import Grid from '@mui/material/Grid';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const SearchBars = ({searched,requestSearch}) => {
 
   return (
       <>
       <Grid container justifyContent="center">
-        <h2>Material UI</h2>
+        <Grid item xs={6}>
+          <h2>Material UI</h2>
+        </Grid>
+        <Grid item xs={4}>
+        <a href="https://www.linkedin.com/in/hitesh-sharma-creator/" target='blank'><LinkedInIcon /></a>
+        <a href="https://github.com/hiteshcreator/vite-react-app/" target='blank' style={{marginLeft:'5px'}}><GitHubIcon /></a>
+        </Grid>
       </Grid>
         
       <TextField id="outlined-basic"  variant="outlined"
